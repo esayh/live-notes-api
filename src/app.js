f2d381cb9005770037f93b6fff3ce323981662e6
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-04-26 17:18:33 -0400

    test API with dummy data

--- src/app.js
+++ src/app.js
@@ -5,17 +5,62 @@ const { ApolloServer, gql } = require("apollo-server-express");
 // Run server on specified port
 const port = process.env.PORT || 3003;
 
+const notes = [
+  {
+    id: "1",
+    content:
+      "If you can't explain it to a six year old, you don't understand it yourself.",
+    author: "Albert Einstein",
+  },
+  {
+    id: "2",
+    content:
+      "The man who does not read has no advantage over the man who cannot read.",
+    author: "Mark Twain",
+  },
+  {
+    id: "3",
+    content: "The future belongs to those who prepare for it today.",
+    author: "Malcolm X",
+  },
+];
+
 // GraphQL Schema
 const typeDefs = gql`
   type Query {
-    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 // Resolver for Schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello World!",
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find((note) => note.id === args.id);
+    },
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteVal = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: "Muhammad Ali",
+      };
+      notes.push(noteVal);
+      return noteVal;
+    },
   },
 };
 
