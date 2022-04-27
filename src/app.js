5634faa15a9a5a1fdd8320f2172ae8ba74a7a541
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-04-26 23:50:54 -0400

    isolated resolvers to its own file and updated app.js

--- src/app.js
+++ src/app.js
@@ -3,33 +3,15 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 require("dotenv").config();
 
+// imported modules
 const db = require("./db");
 const models = require("./models");
 const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
 // Run server on specified port
 const DB_HOST = process.env.DB_HOST;
 const port = process.env.PORT || 3003;
-
-// Resolver for Schema fields
-const resolvers = {
-  Query: {
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    },
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: "Muhammad Ali",
-      });
-    },
-  },
-};
 
 // Config
 const app = express();
@@ -38,7 +20,13 @@ const app = express();
 db.connect(DB_HOST);
 
 // Apollo server
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  },
+});
 
 // Apollo GraphQL middleware
 server.start().then((res) => {
