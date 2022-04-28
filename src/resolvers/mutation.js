6ea5f6fd24e433dca882bc9c869fa37e4857618b
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-04-27 20:24:09 -0400

    added signUp and signIn mutations w/ bcrypt and jwt

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,13 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {
+  AuthenticationError,
+  ForbiddenError,
+} = require("apollo-server-express");
+require("dotenv").config();
+
+const gravatar = require("../util/avatar");
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -27,5 +37,45 @@ module.exports = {
         new: true,
       }
     );
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+    email = email.trim().toLowerCase();
+
+    // hash password
+    const hashed = await bcrypt.hash(password, 10);
+    const avatar = gravatar(email);
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed,
+      });
+
+      // create jwt
+      return jwt.sign({ id: user._id }, process.env.JW_TOKEN);
+    } catch (err) {
+      throw new Error("There was an error creating account");
+    }
+  },
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    // if user is not valid throw error
+    if (!user) {
+      throw new AuthenticationError("Error signing in");
+    }
+    // if password is not valid throw error
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError("Error signing in");
+    }
+    return jwt.sign({ id: user._id }, process.env.JW_TOKEN);
   },
 };
