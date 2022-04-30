3c9b47d29ac9e7552ed6bcd879509e03f52ca7ef
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-04-30 00:29:19 -0400

    updated schema and query to include notefeed limit

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -14,4 +14,37 @@ module.exports = {
   me: async (parent, args, { models, user }) => {
     return await models.User.findById(user.id);
   },
+  noteFeed: async (parent, { cursor }, { models }) => {
+    // limit on notes feed
+    const limit = 10;
+
+    // default next page val is false
+    let hasNextPage = false;
+
+    let cursorQuery = {};
+
+    if (cursor) {
+      cursorQuery = { _id: { $lt: cursor } };
+    }
+    // get notes from db sorted from new - old
+    let notes = await models.Note.find(cursorQuery)
+      .sort({ _id: -1 })
+      .limit(limit + 1);
+
+    // if notes exceeds limit,
+    // set nextpage to true and trim notes to the limit
+    if (notes.length > limit) {
+      hasNextPage = true;
+      notes = notes.slice(0, -1);
+    }
+
+    // the new cursor will be the obj ID of the last note in our feed arr
+    const newCursor = notes[notes.length - 1]._id;
+
+    return {
+      notes,
+      cursor: newCursor,
+      hasNextPage,
+    };
+  },
 };
